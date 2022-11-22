ee902d9be4ceb1e65e745fb09987a7c3f3e568d7
DuongLee <89838052+LeeDuonng@users.noreply.github.com>
2022-11-22 14:23:14 +0700

    first commit

---
+++ Lab1/Nhom10_Lab1.sql
@@ -0,0 +1,7 @@
+﻿select * from NHANVIEN;
+--select * from NHANVIEN where PHG = 4;
+--select * from NHANVIEN where LUONG > 30000;
+--select * from NHANVIEN where (LUONG > 25000 and PHG = 4) or (LUONG > 30000 and PHG = 5);
+--select CONCAT(HONV,  ' ', TENLOT, ' ', TENNV) from NHANVIEN where DCHI like N'%HCM%' or DCHI like N'%Hồ Chí Minh%';
+--select CONCAT(HONV,  ' ', TENLOT, ' ', TENNV) from NHANVIEN where HONV like N'N%';
+--select NGSINH, DCHI from NHANVIEN where HONV = N'Đinh' and TENLOT = N'Bá' and TENNV = N'Tiên';
